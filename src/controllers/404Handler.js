78b9a02aacf36316f5bcbf4112cc9e6386b7b3e5
Elias Biondo <contato@eliasbiondo.com>
2021-01-20 15:43:07 -0300

    :hammer: hard reset

--- src/controllers/404Handler.js
+++
@@ -1,5 +0,0 @@
-const notFoundErrorHandler = (req, res) => {
-    res.status(404).send('404 - Not Found')
-}
-
-module.exports = notFoundErrorHandler;
--- src/controllers/api/index.js
+++
@@ -1,5 +0,0 @@
-const API_IndexController = (req, res) => {
-    res.status(200).json({Status: 'Online'})
-}
-
-module.exports = API_IndexController;
