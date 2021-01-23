2a13755b5040d28743d1d3816963e47d291f7980
Elias Biondo <contato@eliasbiondo.com>
2021-01-23 11:34:27 -0300

    :hammer: Hard reset

--- src/routes/api/auth/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const authController = require("../../../controllers/api/authController");
-
-router.post('/users', authController.store)
-
-module.exports = router;
